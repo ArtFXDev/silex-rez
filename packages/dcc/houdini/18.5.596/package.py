133a16576891f69fef146c772860668ebf77bbb4
OlivierArgentieri <olivierargentieri@gmail.com>
2021-11-08 09:07:39 +0100

    Update package.py

--- packages/dcc/houdini/18.5.596/package.py
+++ packages/dcc/houdini/18.5.596/package.py
@@ -1,9 +1,6 @@
 name = "houdini"
 version = "18.5.596"
 
-requires = [
-    "python-3.7",
-]
 tools = [
     "pyside2-rcc",
     "rcc",
@@ -15,14 +12,16 @@ tools = [
     "hython",
 ]
 
-variants = [["platform-windows"]]
-
 def commands():
     # Set this variable to your houdini install path
     houdini_install_path = "C:/Houdini18"
-
+    env.HFS.set(houdini_install_path)
     env.PATH.append(f"{houdini_install_path}/bin")  # noqa
     env.PYTHONPATH.append(f"{houdini_install_path}/python37/lib/site-packages")  # noqa
     env.PYTHONPATH.append(f"{houdini_install_path}/houdini/python3.7libs")  # noqa
     env.PATH.append(f"{houdini_install_path}/toolkit/include")  # noqa
-    env.HOUDINI_PATH.append(f"{houdini_install_path}/houdini")
+    env.HOUDINI_PATH.append(f"{houdini_install_path}/houdini") # noqa
+
+    env.HOUDINI_NO_ENV_FILE = 1 # noqa
+    env.HOUDINI_PACKAGE_SKIP = 1 # noqa
+
