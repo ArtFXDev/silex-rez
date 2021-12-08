57e2b17b17953d1011c53117429a21b5b20b04e9
GuillaumeRoeder <55831311+GuillaumeRoeder@users.noreply.github.com>
2021-12-08 15:30:09 +0100

    feature, env houdini in fady package

--- packages/5rn/fady/package.py
+++ packages/5rn/fady/package.py
@@ -13,3 +13,9 @@ timestamp = 1635410671
 vcs = 'git'
 
 format_version = 2
+
+
+
+def command():
+    env.HOUDINI_PATH.append('"C:/Users/etudiant/Documents/houdini18.5/DMNK-Tools-master"') 
+    env.HOUDINI_PATH.append("C:/Users/etudiant/Documents/houdini18.5/MOPS") 
