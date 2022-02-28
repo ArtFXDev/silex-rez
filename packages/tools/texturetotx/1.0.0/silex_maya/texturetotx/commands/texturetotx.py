c5373f59d1ecedbf5d213ab5abb78e0abf1c2d4f
OlivierArgentieri <olivierargentieri@gmail.com>
2022-02-28 12:02:31 +0100

    test with existing tx ok, fix for aces parameters

--- packages/tools/texturetotx/1.0.0/silex_maya/texturetotx/commands/texturetotx.py
+++ packages/tools/texturetotx/1.0.0/silex_maya/texturetotx/commands/texturetotx.py
@@ -1,4 +1,3 @@
-
 from __future__ import annotations
 from cmath import exp
 
@@ -28,28 +27,60 @@ from maya import cmds
 
 
 class TextureToTx(CommandBase):
+    """
+    Convert texture to the scene to tx
+    """
+
+    parameters = {
+        "keep_existing_tx": {
+            "label": "Keep existing tx",
+            "type": bool,
+            "value": False,
+        }
+    }
 
     async def prompt_filepath(self, file_paths, action_query):
-        # Create a new parameter to prompt label
+        """
+        Info box on action
+        """
         info_parameter = ParameterBuffer(
             type=TextParameterMeta("info"),
             name="info",
             label="Info",
-            value="Theses path will be convert to tx :\n\n {}".format('\n'.join(file_paths)),
+            value="Theses path will be convert to tx :\n\n {}".format(
+                "\n".join(file_paths)
+            ),
         )
 
-        await self.prompt_user(action_query, { "info": info_parameter})
+        await self.prompt_user(action_query, {"info": info_parameter})
 
     def get_textures_file_path(self):
-        return { node: cmds.getAttr(f"{node}.fileTextureName") for node in cmds.ls(type="file")  }
-    
+        """
+        Return texturepath for each maya node file
+        """
+        return {
+            node: {
+                "file": cmds.getAttr(f"{node}.fileTextureName"),
+                "aces": cmds.getAttr(f"{node}.colorSpace"),
+            }
+            for node in cmds.ls(type="file")
+        }
+
     def get_expand_path(self, file_path: Path):
+        """
+        return expand_path from utils
+        """
         return files.expand_path(file_path)
 
     def get_version_path(self, file_path: str):
+        """
+        Search and return the version folder.
+        Like this: P:/test_pipe/shots/s01/p010/storyboard_main/publish/v000
+        """
+
         file_path = Path(file_path)
         ext = file_path.suffix
-        ext = ext.replace('.','')
+        ext = ext.replace(".", "")
         expand_path = self.get_expand_path(file_path)
         ext = expand_path["OutputType"]
         version_path = file_path.parent
@@ -59,20 +90,24 @@ class TextureToTx(CommandBase):
 
         return version_path.parent
 
-    def set_file_texture_attribute(self, node, value):
-        cmds.setAttr(f"{node}.fileTextureName", value, type="string")
+    def set_texture_attribute(self, attribute, node, value):
+        """
+        Set path to node file maya.
+        """
+        cmds.setAttr(f"{node}.{attribute}", value, type="string")
 
-    async def exec_make_tx(self, input_file:str, out_file:str, logger):
+    async def exec_make_tx(self, input_file: str, out_file: str, logger):
+        """
+        Launch the maketx process to convert tx in background
+        """
         batch_cmd = (
-            command_builder.CommandBuilder(
-                "maketx", delimiter=None
-            )
+            command_builder.CommandBuilder("maketx", delimiter=None)
             .param("o", out_file)
             .value(input_file)
         )
-
+        logger.error(batch_cmd)
         await thread_client.execute_in_thread(
-            subprocess.call, batch_cmd.as_argv(), shell=True
+            subprocess.call, batch_cmd.as_argv(), shell=False
         )
 
     @CommandBase.conform_command()
@@ -83,27 +118,49 @@ class TextureToTx(CommandBase):
         logger: logging.Logger,
     ):
 
+        # get paramters
+        keep_existing_tx = parameters["keep_existing_tx"]
+
         # Export obj to fbx
         file_nodes_paths = await execute_in_main_thread(self.get_textures_file_path)
         logger.error(file_nodes_paths)
-        file_nodes_paths = { n: p for n, p in file_nodes_paths.items() if Path(p).suffix != ".tx" }
+        logger.error(file_nodes_paths.items())
+
+        file_nodes_paths = {
+            n: p for n, p in file_nodes_paths.items() if Path(p["file"]).suffix != ".tx"
+        }
         logger.error(file_nodes_paths)
 
-        file_paths = [ p for n, p in file_nodes_paths.items() ]
-        await self.prompt_filepath(file_paths, action_query)
+        file_paths = [p["file"] for p in file_nodes_paths.values()]
+        if len(file_paths) > 0:
+            await self.prompt_filepath(file_paths, action_query)
 
         # prompt path files
-        for node, file in file_nodes_paths.items():
+        for node, temp_object in file_nodes_paths.items():
+            file = temp_object["file"]
+            aces = temp_object["aces"]
             version_path = self.get_version_path(file)
             expand_path = self.get_expand_path(Path(file))
             out_file_name = Path(file).with_suffix(".tx")
             out_file_name = out_file_name.name
             final_path = version_path / "tx" / expand_path["Name"] / out_file_name
             logger.error(final_path)
+            logger.error(os.path.isfile(final_path))
 
-        # exec maektx
-        await self.exec_make_tx(file, final_path, logger)
+             # create out dir if not exist
+            if not os.path.exists(final_path.parent):
+                os.makedirs(final_path.parent)
 
-        # set file attribute
-        if os.path.isfile(final_path):
-            await execute_in_main_thread(self.set_file_texture_attribute, node, file)
+            # If keep existing and tx already exist
+            if not os.path.isfile(final_path) or not keep_existing_tx:
+                # exec maektx
+                await self.exec_make_tx(file, final_path, logger)
+
+            # test if export completed and set texture data
+            if os.path.isfile(final_path):
+                await execute_in_main_thread(
+                    self.set_texture_attribute, "fileTextureName", node, final_path
+                )
+                await execute_in_main_thread(
+                    self.set_texture_attribute, "colorSpace", node, aces
+                )
