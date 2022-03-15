daaa317288400db426aa8f9ff457a2f50b3d1949
ArtFXTD <td@PROD.artfx.fr>
2022-03-15 01:24:48 +0100

    mount p add exceptions

--- packages/utils/mount_render_drive/1.0.0/platform-windows/mount_rd.ps1
+++ packages/utils/mount_render_drive/1.0.0/platform-windows/mount_rd.ps1
@@ -1,13 +1,8 @@
-$letter = "P"
-$user = $env:SERVER_USER
+$USER = $env:SERVER_USER
 $PWD = $env:SERVER_PASS
 
 $path = ("\\{0}" -f $args[0])
 $path += "\PIPELINE"
-
-Write-Output "BEFORE DELETE"
-net use
-Write-Output "---------------------------------"
 
 Write-Output "DELETE *"
 net use * /delete /yes
@@ -17,10 +12,21 @@ If ((Get-PSDrive).Name -eq 'PIPELINE' -or (Get-PSDrive).Name -eq 'P') {
 	Write-Output "TODO"
 }
 
-Write-Output "USE P AND MARVIN"
-net use P: $path /user:$user $PWD
-net use Z: \\marvin\installers /USER:etudiant artfx2020
-Write-Output "---------------------------------"
+try {
+	Write-Output "NET USE P"
+	net use P: $path /user:$USER $PWD /persistent:no
+	Write-Output "---------------------------------"
+	
+	if ($LASTEXITCODE -eq 0) {
+        write-host "net use successful"
+    } else {
+        throw $error[0].Exception
+    }
+} catch {
+	Write-Output "ERROR MOUNTING P:"
+	Write-Output $error[0].Exception
+	exit 1
+}
 
 Write-Output "AFTER MOUNT P"
 net use
@@ -29,5 +35,6 @@ Write-Output "---------------------------------"
 Write-Output "LS P"
 ls P:\
 Write-Output "---------------------------------"
+
 
 # New-SmbMapping -LocalPath 'P:' -RemotePath (Convert-Path $path) -UserName $user -Password $PWD
