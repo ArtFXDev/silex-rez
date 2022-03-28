e7d5fb070dd03c71d6b05e101465c0a462e496da
ArtFXTD <td@PROD.artfx.fr>
2022-03-28 10:26:46 +0200

    xgen package

---
+++ packages/softwares/xgen/1.0/package.py
@@ -0,0 +1,7 @@
+name = "xgen"
+version = "1.0"
+
+def commands():
+    # See: https://docs.arnoldrenderer.com/display/A5AFMUG/Using+kick+to+render+ASS+files+with+XGen
+    env.PATH.append(r"C:\Maya2022\Maya2022\bin")
+    env.PATH.append(r"C:\Maya2022\Maya2022\plug-ins\xgen\bin")
