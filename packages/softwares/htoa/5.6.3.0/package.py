728056fdc3138cac4814433af7903af1f8e5eb82
OlivierArgentieri <olivierargentieri@gmail.com>
2022-03-31 11:19:48 +0200

    rename houdini18.5 package to 18.5.596

--- packages/softwares/htoa/5.6.3.0/package.py
+++ packages/softwares/htoa/5.6.3.0/package.py
@@ -13,6 +13,10 @@ tools = [
     "oslinfo",
 ]
 
+requires = [
+    "houdini-18.5.596"
+]
+
 def commands():
     env.PATH.prepend(r"{root}/htoa-5.6.3.0_ra766b1f_houdini-18.5.596/htoa-5.6.3.0_ra766b1f_houdini-18.5.596/scripts/bin")
     env.HOUDINI_PATH.prepend(r"{root}/htoa-5.6.3.0_ra766b1f_houdini-18.5.596/htoa-5.6.3.0_ra766b1f_houdini-18.5.596")
