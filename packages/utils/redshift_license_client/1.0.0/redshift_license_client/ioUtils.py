c6c568e664e00b094491b44a0b7e649f5acd015e
ArtFXTD <td@PROD.artfx.fr>
2022-05-11 23:00:09 +0200

    increase buffer size

--- packages/utils/redshift_license_client/1.0.0/redshift_license_client/ioUtils.py
+++ packages/utils/redshift_license_client/1.0.0/redshift_license_client/ioUtils.py
@@ -1,27 +1,32 @@
-from logging import config
-import os
-import socket
 import json
+import os
 import platform
+import socket
+from logging import config
 
 # Config ---------------------
+
 
 def config_folder_path():
     appdata = os.getenv("LOCALAPPDATA")
     return os.path.join(appdata, "redshift_license")
 
+
 def active_license_file_path():
     return os.path.join(config_folder_path(), "active.rslic")
+
 
 def create_config_folder():
     config_path = config_folder_path()
     if not os.path.exists(config_path):
         os.makedirs(config_path)
 
+
 def save_active_license_key(licenseKey):
     with open(active_license_file_path(), "wt") as license:
         license.write(licenseKey)
         license.flush()
+
 
 def get_active_license_key():
     if not os.path.exists(active_license_file_path()):
@@ -29,16 +34,19 @@ def get_active_license_key():
     with open(active_license_file_path(), "rt") as license:
         return license.read()
 
+
 def clean_active_license_key():
     with open(active_license_file_path(), "wt") as license:
         license.write("")
         license.flush()
 
+
 # -----------------------------
 
 # Server communication --------
 
-def communicate_server(addr, data, recvBufferSize=4096):
+
+def communicate_server(addr, data, recvBufferSize=8192):
     retData = b""
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect(addr)
@@ -46,9 +54,12 @@ def communicate_server(addr, data, recvBufferSize=4096):
         retData = sock.recv(recvBufferSize)
     return retData
 
+
 def server_get_license_key(addr):
     request = {"CMD": "GET_LIC_KEY", "NODE": platform.node()}
-    response = json.loads( communicate_server( addr, json.dumps(request).encode() ).decode() )
+    server_response = communicate_server(addr, json.dumps(request).encode())
+
+    response = json.loads(server_response.decode())
     if "SUCCESS" in response.keys():
         if response["SUCCESS"]:
             return response["KEY"]
@@ -56,9 +67,12 @@ def server_get_license_key(addr):
             return None
     return None
 
+
 def server_get_license_info(addr, licenseKey):
     request = {"CMD": "GET_LIC_INFO", "KEY": licenseKey}
-    response = json.loads( communicate_server( addr, json.dumps(request).encode() ).decode() )
+    response = json.loads(
+        communicate_server(addr, json.dumps(request).encode()).decode()
+    )
     if "SUCCESS" in response.keys():
         if response["SUCCESS"]:
             return response["INFO"]
@@ -66,30 +80,46 @@ def server_get_license_info(addr, licenseKey):
             return None
     return None
 
+
 def server_release_license_key(addr, licenseKey):
     request = {"CMD": "RELEASE_LIC", "KEY": licenseKey}
-    response = json.loads( communicate_server( addr, json.dumps(request).encode() ).decode() )
+    response = json.loads(
+        communicate_server(addr, json.dumps(request).encode()).decode()
+    )
     if "SUCCESS" in response.keys():
         return response["SUCCESS"]
     return None
 
+
 def server_add_license(addr, user, password, instance):
-    request = {"CMD": "ADD_LIC", "USER": user, "PASSWORD": password, "INSTANCE": instance}
-    response = json.loads( communicate_server( addr, json.dumps(request).encode() ).decode() )
+    request = {
+        "CMD": "ADD_LIC",
+        "USER": user,
+        "PASSWORD": password,
+        "INSTANCE": instance,
+    }
+    response = json.loads(
+        communicate_server(addr, json.dumps(request).encode()).decode()
+    )
     if "SUCCESS" in response.keys():
         return response["SUCCESS"]
     return None
+
 
 def server_remove_license(addr, licenseKey):
     request = {"CMD": "REMOVE_LIC", "KEY": licenseKey}
-    response = json.loads( communicate_server( addr, json.dumps(request).encode() ).decode() )
+    response = json.loads(
+        communicate_server(addr, json.dumps(request).encode()).decode()
+    )
     if "SUCCESS" in response.keys():
         return response["SUCCESS"]
     return None
 
+
 def server_list_licenses(addr):
     request = {"CMD": "LIST_LIC"}
-    response = json.loads( communicate_server( addr, json.dumps(request).encode() ).decode() )
+    server_response = communicate_server(addr, json.dumps(request).encode())
+    response = json.loads(server_response.decode())
     if "SUCCESS" in response.keys():
         if response["SUCCESS"]:
             return response["LICENSES"]
